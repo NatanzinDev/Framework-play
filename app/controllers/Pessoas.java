0fcc4b32e695c19f7084b3fb26ba00055b5d8b2b
Natan Paulo <natanpaulo1300@gmail.com>
2025-06-11 16:09:37 -0300

    Pesquisa com o parametro na action

--- app/controllers/Pessoas.java
+++ app/controllers/Pessoas.java
@@ -12,44 +12,35 @@ public class Pessoas extends Controller {
 		render(p);
 	}
 
-	public static void lista(Boolean a) {
-		String busca = params.get("busca");
-	
+	public static void lista(String busca) {
+		
+
 		List<Pessoa> lista;
-		
-		if(busca == null) {
+
+		if (busca == null) {
 			lista = Pessoa.findAll();
-		}else {
-			lista = Pessoa.find("select p from Pessoa p where p.nome like ?1 or p.email like ?1", "%"+busca+"%").fetch();
+		} else {
+			lista = Pessoa.find("lower(nome) like ?1 or lower(email) like ?1", "%" + busca + "%").fetch();
 		}
-		
+
 		render(lista);
 
 	}
 
 	public static void salvar(Pessoa p) {
-		if(!p.nome.isEmpty() ) {
+		if (!p.nome.isEmpty()) {
 			p.nome = p.nome.toUpperCase();
 		}
-		if(!p.email.isEmpty()) {
+		if (!p.email.isEmpty()) {
 			p.email = p.email.toLowerCase();
 		}
-		
-		String msg = "";
-		boolean confirma = false;
-		try {
-			p.save();
-			 confirma = true;
-			 
-		}catch(Exception e ) {
-			confirma = false;
-		}finally {
-			lista(confirma);
-		}
-		
-		
 
 		
+
+		p.save();
+
+		lista(null);
+
 	}
 
 	public static void form() {
@@ -58,18 +49,9 @@ public class Pessoas extends Controller {
 
 	public static void excluir(long id) {
 		Pessoa p = Pessoa.findById(id);
-		
-		Boolean a = true;
-		
-		try {
-			p.delete();
-			a = true;
-		}catch(Exception e ) {
-			a = false;
-		}finally {
-			lista(a);
-		}
-	
+		p.delete();
+		lista(null);
+
 	}
 
 	public static void editar(long id) {
